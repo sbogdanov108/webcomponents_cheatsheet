66e3d79b877e0f6c68fb347556ad59e6dae53876
Sergey Bogdanov <bgrodthai@gmail.com>
2017-03-25 15:09:51 +0300

    Main commit

---
+++ 03-Styles/02-Import/02-Sample/JavaScript.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener('load', function() {
+  // Получение доступа к элементу link и чтение содержимого через свойство import
+  var content = document.querySelector("#list-import").import;
+  var list    = content.querySelector("ul");
+
+  document.body.appendChild(list);
+});
